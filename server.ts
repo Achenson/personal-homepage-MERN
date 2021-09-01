c02d6b5cc96ea3439158df0ad8c141b5b97064a5
Achenson <ach.01.mail@gmail.com>
2021-09-01 16:20:01 +0200

    db connection

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const {graphqlHTTP} = require("express-graphql");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema.ts");
 
 const app = express();
@@ -31,17 +32,18 @@ app.use(
   })
 );
 
-// dotenv.config();
+dotenv.config();
 
-// const MONGODB_CONNECTION_STRING = process.env.DB;
+const MONGODB_CONNECTION_STRING = process.env.DB;
 
-// mongoose
-//   .connect(MONGODB_CONNECTION_STRING, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("connection successful"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection successful"))
+  // .catch((err: Mon) => console.log(err));
+  .catch(() => console.log("err"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!4");
