925b5d54eafa549715e8edfa9cfe140e44e3d8d3
Andrzej Chramiec-Głąbik <ach.01.mail@gmail.com>
2021-09-28 14:22:03 +0200

    urlql init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 // import { DndProvider } from "react-dnd";
 import { DndProvider } from "react-dnd-multi-backend";
-import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch';
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
+import { createClient, Provider } from "urql";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import Main from "./components/Main";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+});
 
 // import { HTML5Backend } from "react-dnd-html5-backend";
 // import { TouchBackend } from 'react-dnd-touch-backend';
 
 // import { ReactQueryDevtools} from "react-query-devtools";
-import {QueryClientProvider, QueryClient} from "react-query";
-import Main from "./components/Main";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <Provider value={client}>
+      <QueryClientProvider client={queryClient}>
         <div className="App">
-        {/* <DndProvider backend={HTML5Backend}> */}
-        <DndProvider options={HTML5toTouch}>
-          <Main />
-        </DndProvider>
-      </div>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-    
+          {/* <DndProvider backend={HTML5Backend}> */}
+          <DndProvider options={HTML5toTouch}>
+            <Main />
+          </DndProvider>
+        </div>
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
