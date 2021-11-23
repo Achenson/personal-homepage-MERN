e31eaf0df7cf6b4d5b6135a5bceb249e2bb24679
Andrzej Chramiec-Głąbik <ach.01.mail@gmail.com>
2021-11-23 16:04:19 +0100

    upload background image frontend nowrap

--- client/src/components/UpperUI/BackgroundSettings_Upload.tsx
+++ client/src/components/UpperUI/BackgroundSettings_Upload.tsx
@@ -20,9 +20,10 @@ function BackgroundSettings_Upload({
 
   const [uploadFile, setUploadFile] = React.useState<Blob>();
 
-  const [changeSettingsResult, changeSettings] = useMutation<any, SettingsDatabase_i>(
-    ChangeSettingsMutation
-  );
+  const [changeSettingsResult, changeSettings] = useMutation<
+    any,
+    SettingsDatabase_i
+  >(ChangeSettingsMutation);
 
   let uploadFileName;
   // @ts-ignore
@@ -68,43 +69,41 @@ function BackgroundSettings_Upload({
         globalSettings.picBackground ? "" : "hidden"
       }`}
     >
-      
-        <button
-          className={`border border-${uiColor} rounded-md px-1 pb-px hover:bg-${uiColor} hover:bg-opacity-50 transition-colors duration-150
+      <button
+        className={`border border-${uiColor} rounded-md px-1 pb-px hover:bg-${uiColor} hover:bg-opacity-50 transition-colors duration-150
         focus:outline-none focus-visible:ring-1 ring-${uiColor}`}
-          style={{ height: "26px" }}
-          onClick={(e) => {
-            e.preventDefault();
-            console.log("click");
-            hiddenFileInput.current?.click();
-          }}
-        >
-          Browse...
-        </button>
-        <div
-          className={`bg-blueGray-50 pl-px ${
-            xsScreen ? "w-32" : "w-44"
-          } border border-gray-300 align-text-bottom`}
-          style={{ height: "26px" }}
-        >
-          <p className="overflow-hidden">{uploadFileName}</p>
-        </div>
+        style={{ height: "26px" }}
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("click");
+          hiddenFileInput.current?.click();
+        }}
+      >
+        Browse...
+      </button>
+      <div
+        className={`bg-blueGray-50 pl-px ${
+          xsScreen ? "w-32" : "w-44"
+        } border border-gray-300 align-text-bottom`}
+        style={{ height: "26px" }}
+      >
+        <p className="overflow-hidden whitespace-nowrap">{uploadFileName}</p>
+      </div>
 
-        <input
-          type="file"
-          name="file"
-          // accept="image/x-png,image/jpeg,image/gif"
-          /*     className={`bg-blueGray-50 h-6 ${
+      <input
+        type="file"
+        name="file"
+        // accept="image/x-png,image/jpeg,image/gif"
+        /*     className={`bg-blueGray-50 h-6 ${
             xsScreen ? "w-48" : "w-60"
           } border border-gray-300`} */
-          onChange={(e: any) => {
-            setUploadFile(e.target.files[0]);
-            console.log(e.target.files[0]);
-          }}
-          style={{ display: "none" }}
-          ref={hiddenFileInput}
-        />
- 
+        onChange={(e: any) => {
+          setUploadFile(e.target.files[0]);
+          console.log(e.target.files[0]);
+        }}
+        style={{ display: "none" }}
+        ref={hiddenFileInput}
+      />
 
       <button
         type="submit"
