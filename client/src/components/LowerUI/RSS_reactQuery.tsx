161787595c78c3c18271dcc4556128f8e6a27ead
Andrzej Chramiec-Głąbik <ach.01.mail@gmail.com>
2022-04-11 16:13:08 +0200

    rss fetch server side ifinal

--- client/src/components/LowerUI/RSS_reactQuery.tsx
+++ client/src/components/LowerUI/RSS_reactQuery.tsx
@@ -10,9 +10,7 @@ import { ReactComponent as ArrowLeft } from "../../svgs/arrowLeft.svg";
 import { ReactComponent as ArrowRight } from "../../svgs/arrowRight.svg";
 
 import { RssFetchQuery } from "../../graphql/graphqlQueries";
-import {rssExample} from "../../utils/data/rssExample"
-
-
+import { rssExample } from "../../utils/data/rssExample";
 
 // import { useGlobalSettings } from "../../state/hooks/defaultSettingsHooks";
 // import { useRssSettings } from "../../state/hooks/defaultSettingsHooks";
@@ -81,7 +79,6 @@ function ReactQuery({
   //   },
   // });
 
-
   const [rssFetchResults, reexecuteRssFetch] = useQuery({
     query: RssFetchQuery,
     // variables: { userId: authContext.isAuthenticated ? authContext.authenticatedUserId : testUserId },
@@ -89,27 +86,21 @@ function ReactQuery({
     // requestPolicy: 'cache-and-network',
   });
 
-  const {
-    data,
-    fetching,
-    error,
-  } = rssFetchResults;
-
+  const { data, fetching, error } = rssFetchResults;
 
   console.log(currentTab.rssLink);
   console.log(data);
 
   let rssData: any;
+  // let rssData = data.rssFetch.rssFetchData;
 
-  if(data) {
+  if (data) {
     rssData = data.rssFetch.rssFetchData;
-  } else {
-    rssData = rssExample.data.rssFetch.rssFetchData;
-  }
+  } 
   
-
- 
-
+  // else {
+  //   rssData = rssExample.data.rssFetch.rssFetchData;
+  // }
 
   // async function fetchFeed() {
 
@@ -120,7 +111,7 @@ function ReactQuery({
   //     // let response = await parser.parseURL(currentTab?.rssLink);
   //     // return response;
   //     let toSendUrl = encodeURIComponent(`${currentTab.rssLink}`);
-    
+
   //     let response = await fetch(baseFetchUrl + toSendUrl);
 
   //     if (!response.ok) {
@@ -132,7 +123,7 @@ function ReactQuery({
   //     // let newResponse = await parser.parseURL(extendedRSSurl);
   //     // return newResponse;
   //     let newToSendUrl = encodeURIComponent(extendedRSSurl);
-   
+
   //     let newResponse = await fetch(baseFetchUrl + newToSendUrl);
 
   //     if (!newResponse.ok) {
@@ -143,7 +134,7 @@ function ReactQuery({
   //   }
   // }
 
-    function mapData() {
+  function mapData() {
     let arrOfObj = [];
 
     let howManyNews = rssData.items.length;
@@ -210,7 +201,7 @@ function ReactQuery({
 
   function lastPageNumber() {
     // if (status !== "success") {
-    if (!data) {
+    if (!rssData) {
       return 1;
     }
 
@@ -249,20 +240,20 @@ function ReactQuery({
 
         <button
           className="focus-2-inset"
-        onClick={() => {
+          onClick={() => {
             if (pageNumber < lastPageNumber()) {
               setPageNumber(pageNumber + 1);
             }
-          }} 
+          }}
           aria-label={"Next page"}
         >
-            <ArrowRight
+          <ArrowRight
             className={`h-8 ${
               pageNumber === lastPageNumber()
                 ? `text-gray-400`
                 : `cursor-pointer text-black hover:bg-gray-200`
             }`}
-          /> 
+          />
         </button>
       </div>
       <div
