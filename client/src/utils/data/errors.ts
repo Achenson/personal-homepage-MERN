a3353c424e537e8b70cd10a18fcef3256f8932f8
Achenson <ach.01.mail@gmail.com>
2022-09-05 20:34:26 +0200

    titles format change init

--- client/src/utils/data/errors.ts
+++ client/src/utils/data/errors.ts
@@ -2,11 +2,13 @@ import { BookmarkErrors, TabErrors } from "../interfaces";
 
 export const bookmarkErrors = {
   titleFormat:
-    "Title can contain letters, numbers, underscores and single spaces",
+    // "Title can contain letters, numbers, underscores and single spaces",
+    "Title should start and end with a letter, digit or underscore. Only single spaces are allowed",
   titleUniqueness: "Bookmark with that title already exists",
   // tagFormat: "Folders should consist of words separated by coma and single space",
   tagFormat:
-    "Folders should consist of letters, numbers, underscores, single spaces separated by coma and space",
+    // "Folders should consist of letters, numbers, underscores, single spaces separated by coma and space",
+    "Enter folder titles (starting and ending with a letter, digit or underscore, single spaces are allowed), separated by coma and space",
   noteError: "Folder names cannot be the same as Note titles",
   rssError: "Folder names cannot be the same as RSS titles",
   tagRepeat: "Each folder should be unique",
@@ -27,13 +29,15 @@ export const bookmarkErrorsAllFalse: BookmarkErrors = {
 export const tabErrors = {
   // titleFormat: "Tab title should consist of words without special characters",
   titleFormat:
-    "Title can contain letters, numbers, underscores and single spaces",
+    // "Title can contain letters, numbers, underscores and single spaces",
+    "Title should start and end with a letter, digit or underscore. Only single spaces are allowed",
   // not used in edit tab
   titleUniqueness: "Tab with that title already exists",
   invalidLinkError: "Invalid link",
   invalidLinkHttpsError: "RSS links should be https only",
   bookmarksFormat:
-    "Bookmarks should consist of letters, numbers, underscores, single spaces separated by coma and space",
+    // "Bookmarks should consist of letters, numbers, underscores, single spaces separated by coma and space",
+    "Enter bookmark titles (starting and ending with a letter, digit or underscore, single spaces are allowed), separated by coma and space",
   bookmarkExistence: "You can choose from existing bookmarks only",
   bookmarksRepeat: "Each bookmark should be unique",
   // note only
