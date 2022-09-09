7886955fa8bb1327f7234557fb2192d66d6247aa
Andrzej Chramiec-Głąbik <ach.01.mail@gmail.com>
2022-09-09 12:17:11 +0200

    escapeRegExp init

--- client/src/utils/regex.ts
+++ client/src/utils/regex.ts
@@ -1,35 +1,51 @@
+// const escapeRegExp = import("lodash.escaperegexp")
+const escapeRegExp = require("lodash.escaperegexp");
 // selectable => either bookmarks of tags
 export function createSelectablesRegex(singleSelectable: string) {
+  let singleSelectableEscaped = escapeRegExp(singleSelectable);
+
   let selectablesRegex = new RegExp(
-    `(^|\\s+|,+|(\\s+,+)*|(,+\\s+)*)${singleSelectable}(\\s+|,+|(\\s+,+)*|(,+\\s+)*|$)`
+    // `(^|\\s+|,+|(\\s+,+)*|(,+\\s+)*)${singleSelectable}(\\s+|,+|(\\s+,+)*|(,+\\s+)*|$)`
+    `(^|\\s+|,+|(\\s+,+)*|(,+\\s+)*)${singleSelectableEscaped}(\\s+|,+|(\\s+,+)*|(,+\\s+)*|$)`
   );
   return selectablesRegex;
 }
 
 // no commas allowed
 export const singleCharRegex =
-// /[\w~`!@#\$%\^&\*\(\)\-\+=\{\}\[\];:'"\\\|<>\./\?]/;
-/[\w~`!@#$%^&*()\-+={}\[\];:'"\\|<>./?]/;
+  // /[\w~`!@#\$%\^&\*\(\)\-\+=\{\}\[\];:'"\\\|<>\./\?]/;
+  /[\w~`!@#$%^&*()\-+={}\[\];:'"\\|<>./?]/;
 
-export function createSelectablesRegex_inverted_start(singleSelectable: string) {
-    let selectablesRegex_inverted_start = new RegExp(`${singleCharRegex.source}+${singleSelectable}`)
-    return selectablesRegex_inverted_start
+export function createSelectablesRegex_inverted_start(
+  singleSelectable: string
+) {
+  let singleSelectableEscaped = escapeRegExp(singleSelectable);
+  let selectablesRegex_inverted_start = new RegExp(
+    `${singleCharRegex.source}+${singleSelectableEscaped}`
+  );
+  return selectablesRegex_inverted_start;
 }
 
 export function createSelectablesRegex_inverted_end(singleSelectable: string) {
-    let selectablesRegex_inverted_end = new RegExp(`${singleSelectable}${singleCharRegex.source}+`)
-    return selectablesRegex_inverted_end;
+  let singleSelectableEscaped = escapeRegExp(singleSelectable);
+  let selectablesRegex_inverted_end = new RegExp(
+    `${singleSelectableEscaped}${singleCharRegex.source}+`
+  );
+  return selectablesRegex_inverted_end;
 }
 
 // title for bookmark or tab
-export const titleRegex = new RegExp(`^${singleCharRegex.source}(\\s?${singleCharRegex.source}+)*$`)
+export const titleRegex = new RegExp(
+  `^${singleCharRegex.source}(\\s?${singleCharRegex.source}+)*$`
+);
 
-export const titleRegex_unflanked = new RegExp(`${singleCharRegex.source}(\\s?${singleCharRegex.source}+)*`)
+export const titleRegex_unflanked = new RegExp(
+  `${singleCharRegex.source}(\\s?${singleCharRegex.source}+)*`
+);
 
 export const tagsOrBookmarksRegexForSaving = new RegExp(
-    `^${titleRegex_unflanked.source}(,\\s${titleRegex_unflanked.source})*,?$`
-  );
-
+  `^${titleRegex_unflanked.source}(,\\s${titleRegex_unflanked.source})*,?$`
+);
 
 /* 
 old code - only /w characters were allowed in titles
